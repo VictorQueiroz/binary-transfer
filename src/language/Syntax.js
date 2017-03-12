e53784ce3156f4e0485dbc58de14ca9241208661
Victor Queiroz <victorcqueirozg@gmail.com>
2017-03-12 01:24:43 -0300

    src/language/Syntax.js: add support for AST.Literal

--- src/language/Syntax.js
+++ src/language/Syntax.js
@@ -1,6 +1,7 @@
 export const Syntax = {
     Schema: 'Schema',
     Vector: 'Vector',
+    Literal: 'Literal',
     TypeBody: 'TypeBody',
     Identifier: 'Identifier',
     CommentBlock: 'CommentBlock',
