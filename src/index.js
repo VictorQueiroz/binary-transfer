6438ada635aafef72a204d8089a1d6ff6ef73c40
Victor Queiroz <victorcqueirozg@gmail.com>
2017-06-18 12:53:25 -0300

    add enums.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export Schema from './Schema';
 export Serializer from './Serializer';
+export * as enums from './enums';
 export Deserializer from './Deserializer';
 export * as language from './language';
